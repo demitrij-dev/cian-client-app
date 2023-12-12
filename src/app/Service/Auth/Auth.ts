554de9c1217e9ba4c3aa75f3a85f9939581ee157
demitrij-dev <mr.dima0816@gmail.com>
2023-12-12 21:08:34 +0700

    v1.0.0

--- src/app/Service/Auth/Auth.ts
+++ src/app/Service/Auth/Auth.ts
@@ -1,15 +1,6 @@
 import axios from "axios";
 
 class Auth{
-    // async reg(email: string, password: string){
-    //     axios.post('http://localhost:8080/server/registration', {email, password})
-    //         .then(response => {
-    //             console.log('Ответ от сервера:', response.data);
-    //         })
-    //         .catch(error => {
-    //             console.error('Ошибка при отправке запроса:', error);
-    //         });
-    // }
     async login(email: string, password: string){
         if(!email) throw "Заполните почту"
         if(!password) throw "Заполните пароль"

8cb5ba6e4b6bb66c7b9b202ca9bac0a7e2ca779f
Dmitry <129826716+demitrij-dev@users.noreply.github.com>
2023-12-13 21:38:22 +0700

    Update Auth.ts

--- src/app/Service/Auth/Auth.ts
+++ src/app/Service/Auth/Auth.ts
@@ -5,7 +5,7 @@ class Auth{
         if(!email) throw "Заполните почту"
         if(!password) throw "Заполните пароль"
 
-        return await axios.post('http://localhost:8080/server/login', {email, password})
+        return await axios.post('https://cian-server-app.onrender.com/server/login', {email, password})
             .then(response => {
                 return response.data
             })
